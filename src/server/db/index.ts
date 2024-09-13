8faf77347e4d1fe5be8f200732e9b5f6cfc8a084
TelarinT <170417670+TelarinT@users.noreply.github.com>
2024-09-13 22:08:27 +0100

    fix few more issues in connecting database

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -1,19 +1,17 @@
-import { createClient, type Client } from "@libsql/client";
-import { drizzle } from "drizzle-orm/libsql";
+import { drizzle, VercelPgClient } from "drizzle-orm/vercel-postgres";
 
 import { env } from "~/env";
 import * as schema from "./schema";
+import { createClient } from "@vercel/postgres";
 
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
  */
 const globalForDb = globalThis as unknown as {
-  client: Client | undefined;
+  client: VercelPgClient | undefined;
 };
-console.log(env.NODE_ENV);
-export const client =
-  globalForDb.client ?? createClient({ url: env.POSTGRES_URL });
+export const client = globalForDb.client ?? createClient();
 if (env.NODE_ENV !== "production") globalForDb.client = client;
 
 export const db = drizzle(client, { schema });
